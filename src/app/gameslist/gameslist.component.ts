22834aeca6cb9ee35a9cfcaf4c0c9a0257538482
ropi59 <mrseed@hotmail.com>
2022-01-20 17:10:14 +0100

    gameslist component added

---
+++ src/app/gameslist/gameslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameslist',
+  templateUrl: './gameslist.component.html',
+  styleUrls: ['./gameslist.component.scss']
+})
+export class GameslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
