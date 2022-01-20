529a2a881e76d4069df3c2dd65ec7f8bc7c8a05e
ropi59 <mrseed@hotmail.com>
2022-01-20 17:12:06 +0100

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { GameslistComponent } from './gameslist/gameslist.component';
 import { HomeComponent } from './home/home.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 import { ProfilListComponent } from './profil-list/profil-list.component';
 import { SendMessageComponent } from './send-message/send-message.component';
 import { UserProfilComponent } from './user-profil/user-profil.component';
 
 const routes: Routes = [
-  { path: 'profillist/:nickname', component: UserProfilComponent },
-  { path: 'profillist', component: ProfilListComponent },
   { path: '', component: HomeComponent },
-  { path: 'send-message', component: SendMessageComponent },
   { path: 'create-account', component: CreateAccountComponent },
+  { path: 'eventslist', component: EventsListComponent },
+  { path: 'my-profile', component: MyProfileComponent },
+  { path: 'profillist', component: ProfilListComponent },
+  { path: 'profillist/:nickname', component: UserProfilComponent },
+  { path: 'gameslist', component: GameslistComponent },
+  { path: 'send-message', component: SendMessageComponent },
+  { path: 'create-event', component: CreateEventComponent },
 ];
 
 @NgModule({
