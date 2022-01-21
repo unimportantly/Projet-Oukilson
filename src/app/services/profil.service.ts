85116472e45e24b6e90bca77076928eb4c292146
ropi59 <mrseed@hotmail.com>
2022-01-21 15:25:26 +0100

    profil service updated unused data deleted for mock

--- src/app/services/profil.service.ts
+++ src/app/services/profil.service.ts
@@ -11,16 +11,12 @@ export class ProfilService {
       nickname: 'ropi59',
       iconUrl:
         'https://upload.wikimedia.org/wikipedia/commons/4/4a/Circle-icons-flame.svg',
-      counters: 50,
-      city: 'Lille',
       online: new Date(),
     },
     {
       id: 2,
       nickname: 'lbmicka',
       iconUrl: '',
-      counters: 150,
-      city: 'Oloron Sainte-Marie',
       online: new Date(),
     },
     {
@@ -28,14 +24,12 @@ export class ProfilService {
       nickname: 'uninportantly',
       iconUrl:
         'https://upload.wikimedia.org/wikipedia/commons/f/f8/Question_mark_alternate.svg',
-      counters: 0,
       online: new Date(),
     },
     {
       id: 4,
       nickname: 'ddidier',
       iconUrl: 'https://pic.clubic.com/v1/images/1498568/raw',
-      counters: 250,
       online: new Date(),
     },
   ];
@@ -62,10 +56,5 @@ export class ProfilService {
     } else {
       return profil;
     }
-  }
-
-  addProfilCountById(profilId: number, profilType: 'count' | 'uncount'): void {
-    const profil = this.getProfilById(profilId);
-    profilType === 'count' ? profil.counters++ : profil.counters--;
   }
 }
