cfb26bf194fd7dce2d5aaca8bdb92469a45a819c
ropi59 <mrseed@hotmail.com>
2022-02-11 17:56:29 +0100

    creator id added with decoded token

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CreateEventService } from './create-event.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-create-event',
@@ -11,29 +12,34 @@ export class CreateEventComponent implements OnInit {
   createEventForm: FormGroup;
   gameButtonContent: string = 'Choisir un jeu';
   townSelected: string = '';
+  creatorId!: string;
 
   constructor(private service: CreateEventService) {
     this.createEventForm = new FormGroup({
-      title: new FormControl("", Validators.required),
-      game: new FormControl("", Validators.required),
-      description: new FormControl("", Validators.required),
-      startingDate: new FormControl("", Validators.required),
+      title: new FormControl('', Validators.required),
+      game: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      startingDate: new FormControl('', Validators.required),
       endingDate: new FormControl(),
-      limitDate: new FormControl("", Validators.required),
-      minPlayer: new FormControl("", Validators.required),
-      maxPlayer: new FormControl("", Validators.required),
-      creator: new FormControl(""),
+      limitDate: new FormControl('', Validators.required),
+      minPlayer: new FormControl('', Validators.required),
+      maxPlayer: new FormControl('', Validators.required),
+      creator: new FormControl(''),
       isPrivate: new FormControl(false),
 
       location: new FormGroup({
-      town: new FormControl("", Validators.required),
-      zipCode: new FormControl("", Validators.required),
-      adress: new FormControl(""),
-    })
+        town: new FormControl('', Validators.required),
+        zipCode: new FormControl('', Validators.required),
+        adress: new FormControl(''),
+      }),
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const token: any = localStorage.getItem('id_token');
+    const tokenDecoded: any = jwt_decode(token);
+    this.creatorId = tokenDecoded.sub;
+  }
   onFormSubmit() {
     let newEvent = { ...this.createEventForm.value };
     this.service.createEvent(newEvent).subscribe((elem) => {
@@ -45,6 +51,6 @@ export class CreateEventComponent implements OnInit {
   chosenGame(selectedGame: { name: string; uuid: string }) {
     this.gameButtonContent = 'Jeu selectionné: ' + selectedGame.name;
     this.createEventForm.patchValue({ game: { uuid: selectedGame.uuid } });
-    this.createEventForm.patchValue({ creator: { nickname: "Toto" } });
+    this.createEventForm.patchValue({ creator: { nickname: this.creatorId } });
   }
 }
