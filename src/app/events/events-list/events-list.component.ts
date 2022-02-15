f6cbd3c0d6bf9bca2a568d500b5ef851b99763db
ropi59 <mrseed@hotmail.com>
2022-02-15 11:29:57 +0100

    search field for events unified with entire app

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -6,32 +6,32 @@ import { EventsPage } from '../events.page';
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
-  styleUrls: ['./events-list.component.scss']
+  styleUrls: ['./events-list.component.scss'],
 })
 export class EventsListComponent implements OnInit {
-
   @Input() events!: Events[];
-  public buttonText = "+";
+  @Input() event!: Event;
+  public buttonText = '+';
 
-  constructor(private eventPage: EventsPage, private eventService: EventService) {
-  }
+  constructor(
+    private eventPage: EventsPage,
+    private eventService: EventService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   /**
    * toggles the event-search and event-details components
    * @param event the event the user wishes to display details of
    */
   switchView(event: Events) {
-    if(this.eventPage.buttonPlus) {
-        this.eventPage.buttonPlus = false;
-        this.buttonText = "-";
-        this.eventService.eventToDetail = event;
-    } 
-    else {
+    if (this.eventPage.buttonPlus) {
+      this.eventPage.buttonPlus = false;
+      this.buttonText = '-';
+      this.eventService.eventToDetail = event;
+    } else {
       this.eventPage.buttonPlus = true;
-      this.buttonText = "+";
+      this.buttonText = '+';
     }
   }
 }
