4f59857495c872d6211e726d6ef0c97ee85bdd79
ropi59 <mrseed@hotmail.com>
2022-02-15 15:42:35 +0100

    lambda user's profil works

--- src/app/user-profil/user-profil.component.ts
+++ src/app/user-profil/user-profil.component.ts
@@ -1,8 +1,11 @@
+import { UserLoggedIn } from './../models/User.model';
+import { MyProfileService } from './../my-profile/my-profile.service';
+import jwt_decode from 'jwt-decode';
+import { ProfilPreviewService } from './../profil-preview/profil-preview.service';
 import { UserProfilService } from './user-profil.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { ProfilService } from '../services/profil.service';
 import { User } from '../models/User.model';
 
 @Component({
@@ -11,30 +14,64 @@ import { User } from '../models/User.model';
   styleUrls: ['./user-profil.component.scss'],
 })
 export class UserProfilComponent implements OnInit {
-  profil!: User;
+  @Input() iconUrl!: string;
+  @Input() profil!: User;
+  @Input() profilLogged!: UserLoggedIn;
+  @Input() friendList!: User[];
+  @Input() deniedList!: User[];
   buttonFriendText!: string;
   buttonDeniedText!: string;
   onFriendList!: boolean;
   onDeniedList!: boolean;
 
   constructor(
-    private profilService: ProfilService,
+    private service: ProfilPreviewService,
+    private myProfilService: MyProfileService,
     private userProfilService: UserProfilService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
+    const token: any = localStorage.getItem('id_token');
+    if (token === null) {
+      this.router.navigate(['login']);
+    }
+    const tokenDecoded: any = jwt_decode(token);
+    this.myProfilService.getProfil(tokenDecoded.sub).subscribe({
+      next: (data) => {
+        this.profilLogged = data;
+        console.log(data);
+      },
+      error: (err) => this.router.navigate(['404']),
+    });
     const profilNickname = this.route.snapshot.params['nickname'];
-    /**this.profil = this.profilService.getProfilByNickname(profilNickname);*/
     this.getProfilByNickname(profilNickname);
     this.onFriendList = false;
     this.onDeniedList = false;
+
+    let ami: User | undefined = undefined;
+    if (this.friendList) {
+      ami = this.friendList.find(
+        (friend) => friend.nickname === this.profil.nickname
+      );
+    }
+
+    let denied: User | undefined = undefined;
+    if (this.deniedList) {
+      denied = this.deniedList.find(
+        (friend) => friend.nickname === this.profil.nickname
+      );
+    }
+
     this.buttonFriendText = 'Ajouter à mes amis';
     this.buttonDeniedText = 'Ajouter à mes indésirables';
-
-    this.profil.iconUrl =
-      'https://upload.wikimedia.org/wikipedia/commons/f/fc/Puzzle.svg';
+    if (this.iconUrl === undefined) {
+      this.iconUrl =
+        'https://upload.wikimedia.org/wikipedia/commons/f/fc/Puzzle.svg';
+    }
+    if (ami) this.onFriendList = true;
+    if (denied) this.onDeniedList = true;
   }
 
   private getProfilByNickname(nickname: string): void {
@@ -48,49 +85,35 @@ export class UserProfilComponent implements OnInit {
     });
   }
 
-  onAddFriendList() {
-    /**ajout a la liste quand on est sur aucune liste */
-    if (
-      this.buttonFriendText == 'Ajouter à mes amis' &&
-      this.buttonDeniedText == 'Ajouter à mes indésirables'
-    ) {
-      this.buttonFriendText = 'Retirer de mes amis';
-      /**retrait de la liste d'amis*/
-    } else if (
-      this.buttonFriendText == 'Retirer de mes amis' &&
-      this.buttonDeniedText == 'Ajouter à mes indésirables'
-    ) {
-      this.buttonFriendText = 'Ajouter à mes amis';
-      this.onFriendList = false;
-      /**ajout a la liste d'amis quand on est sur la liste d'indésirables */
-    } else {
-      this.buttonFriendText = 'Ajouter à mes amis';
-      this.onDeniedList = true;
-    }
+  onAddFriendList(event: Event) {
+    event.stopPropagation();
+    this.onFriendList ? this.removeToFriendList() : this.addToFriendList();
   }
 
-  onAddDeniedList() {
-    /**ajout a la liste d'indésirables quand on est sur aucune liste */
-    if (
-      this.buttonDeniedText == 'Ajouter à mes indésirables' &&
-      this.buttonFriendText == 'Ajouter à mes amis'
-    ) {
-      this.buttonDeniedText = 'Retirer de mes indésirables';
-      /**retirer de la liste d'indésirable */
-    } else if (
-      this.buttonDeniedText == 'Retirer de mes indésirables' &&
-      this.buttonFriendText == 'Ajouter à mes amis'
-    ) {
-      this.buttonDeniedText = 'Ajouter à mes indésirables';
-      this.onDeniedList = false;
-      /**ajout a la liste d'indesirables quand on est sur la liste d'amis */
-    } else {
-      this.buttonDeniedText = 'Ajouter à mes indésirables';
-      this.onFriendList = true;
-    }
+  onAddDeniedList(event: Event) {
+    event.stopPropagation();
+    this.onDeniedList
+      ? console.log('TODO : Supprerssion de la denied')
+      : console.log('TODO : Ajout Denied List');
+  }
+
+  addToFriendList() {
+    this.service.addToFriendlist(this.profil.nickname).subscribe();
+    this.onFriendList = true;
+    this.onDeniedList = false;
+  }
+
+  removeToFriendList() {
+    this.service.removeToFriendList(this.profil.nickname).subscribe();
+    this.onFriendList = false;
+    this.onDeniedList = false;
   }
 
   onContact() {
     this.router.navigateByUrl('send-message');
+  }
+
+  displayDate(date: string): string {
+    return new Date(date).toLocaleString();
   }
 }
